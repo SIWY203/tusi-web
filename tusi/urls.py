619b034bebcae305a39ef77674b8e6c08259cb89
Siwy203 <cefalaspis12345@gmail.com>
2025-05-20 13:29:48 +0200

    css v3 build1

--- tusi/urls.py
+++ tusi/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about_us, name='about_us'),
+    path('about_us/', views.about_us, name='about_us'),
     path('news/', views.news, name='news'),
     path('offer/', views.offer, name='offer'),
     path('gallery/', views.gallery, name='gallery'),
