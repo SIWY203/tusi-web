af8501e644b11277c759685e2153a74a4bda0521
Siwy203 <cefalaspis12345@gmail.com>
2025-05-30 17:35:35 +0200

    moderator added

--- tusiproj/urls.py
+++ tusiproj/urls.py
@@ -4,6 +4,10 @@ from django.conf.urls.static import static
 from filebrowser.sites import site
 from django.conf import settings
 
+admin.site.site_header = "Panel administracyjny TUSI"
+admin.site.site_title = "TUSI Admin"
+admin.site.index_title = "Zarządzanie stroną"
+
 urlpatterns = [
     path("admin/filebrowser/", site.urls),
     path('admin/', admin.site.urls),
