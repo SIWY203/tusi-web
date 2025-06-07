f25b53964719ed751cd37c65a7a892932f4104c9
Siwy203 <cefalaspis12345@gmail.com>
2025-06-07 21:18:53 +0200

    fix mobile

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
-python -m venv .venv
+if ! command -v py &> /dev/null && ! command -v python &> /dev/null
+then
+    echo "Python is not installed. Please install Python 3.x from https://www.python.org/downloads/"
+    exit 1
+fi
+
+py -m venv .venv
 source .venv/Scripts/activate
 pip install -r requirements.txt
 cd tusi-web
-python manage.py runserver
+py manage.py runserver
